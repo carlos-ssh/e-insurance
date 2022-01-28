c87ce9f8c357009bce8bb37c0dc24cc615e0fb0c
Carlos <carlos.anezeh@gmail.com>
2022-01-27 21:24:04 -0600

    Add a style to form controller

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
 
 const Formulario = () => {
   return (
     <form>
-      <div>
-        <label>Brand: </label>
-        <select>
+      <Campo>
+        <Label>Brand</Label>
+        <Select>
           <option value="">-- Select --</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
           <option value="asiatico">Asiatico</option>
-        </select>
-      </div>
-      <div>
-        <label>Year: </label>
-        <select>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Year</Label>
+        <Select>
           <option value="">-- Select --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -26,21 +49,21 @@ const Formulario = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
-      </div>
-      <div>
-        <label>Plan </label>
-        <input
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Plan </Label>
+        <InputRadio
           type="radio"
           name="plan"
           value="basico"
         /> Basic Coverage
-        <input
+        <InputRadio
           type="radio"
           name="plan"
           value="completo"
         /> Full Coverage
-      </div>
+      </Campo>
       <button type="button">Quote</button>
     </form>
   );
